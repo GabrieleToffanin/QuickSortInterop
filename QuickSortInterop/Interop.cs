e00291b3c38d9721e96659b85e19a56db19135b8
Gabriele Toffanin <GabrieleToffanin@outlook.it>
2023-01-29 20:59:57 +0100

    cleanup

--- QuickSortInterop/Interop.cs
+++ QuickSortInterop/Interop.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,15 @@ namespace QuickSortInterop
 {
     public static unsafe partial class Interop
     {
-        [LibraryImport("QuickSort")]
-        public static partial void quickSort(int* buffer, int start, int arrLen);
+        /// <summary>
+        /// Marhsaling Span<int>
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="arrLen"></param>
+
+
+        [LibraryImport("QuickSort", EntryPoint = "quickSort")]
+        [UnmanagedCallConv (CallConvs = new[] {typeof(CallConvCdecl)})]
+        public static partial void QuickSort(Span<int> buffer,int start, int arrLen);
     }
 }
--- QuickSortInterop/Program.cs
+++ QuickSortInterop/Program.cs
@@ -2,17 +2,14 @@
 using System.Runtime.InteropServices;
 using QuickSortInterop;
 
-int[] numerini = { 68, 45, 1, 4, 33, 44, 2,5345,62,34,62457,27,234,57,34567,35678,358,3568,3568 };
+List<int> numerini = new List<int>(){ 68, 45, 1, 4, 33, 44, 2,5345,62,34,62457,27,234,57,34567,35678,358,3568,3568 };
 
-unsafe
+Interop.QuickSort(CollectionsMarshal.AsSpan(numerini),0, numerini.Count - 1);
+
+var numb = numerini.ToArray();
+
+for (int i = 0; i < numerini.Count - 1; i++)
 {
-    fixed(int* ptr = &numerini[0])
-    {
-        Interop.quickSort(ptr, 0, numerini.Length - 1);
-
-        for (int i = 0; i < numerini.Length - 1; i++)
-        {
-            Console.WriteLine(*(ptr + i));
-        }
-    }
+    Console.WriteLine(
+        numb[i]);
 }
